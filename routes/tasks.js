31b5757f9fd6e22f5bbac4aedf30322b64058350
Henry Barnacle <henrybarnacle@yahoo.com>
2017-11-09 22:11:38 -0500

    initial commit

---
+++ routes/tasks.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://henrybarnacle:henrybarnacle@ds251245.mlab.com:51245/mytasklist', ['tasks']);
+
+//get all tasks
+router.get('/tasks', function(req, res, next) {
+	db.tasks.find(function(err, tasks) {
+		if(err) {
+			res.send(err);
+		}
+		res.json(tasks);
+		
+	});
+});
+
+//get single tasks
+router.get('/task/:id', function(req, res, next) {
+	db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+		if(err) {
+			res.send(err);
+		}
+		res.json(task);
+	});
+});
+
+//Save Task
+router.post('/task', function(req, res, next) {
+var task = req.body;
+if(!task.title || (task.isDone + '')) {
+	res.status(400);
+	res,json({
+		"error": "Bad Data"
+	});
+} else {
+	db.tasks.save(task, function(err, task) {
+			if(err) {
+			res.send(err);
+		}
+		res.json(task);
+	 });
+	}
+});
+
+
+//Delete Task
+router.delete('/task/:id', function(req, res, next) {
+	db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+		if(err) {
+			res.send(err);
+		}
+		res.json(task);
+	});
+});
+
+//Update Task
+router.put('/task/:id', function(req, res, next) {
+var task = req.body;
+var updateTask = {};
+
+if(task.isDone) {
+	updateTask.isDOne = task.isDone;
+	}
+
+if (task.title) {
+	updateTask.title = task.title;
+
+	}
+
+	if(!updateTask) {
+		res.status(400);
+		res.json({
+			"error": "Bad Data"
+				});
+
+	} else {
+	db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, updateTask, {}, function(err, task) {
+		if(err) {
+			res.send(err);
+		}
+		res.json(task);
+	});
+	}
+});
+
+
+
+
+
+module.exports = router;
